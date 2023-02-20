e7893857c99715d5270feba8eb31a20236421a47
Vishnu V Unnikrishnan <80766411+vishnuvu-TTCT@users.noreply.github.com>
2023-02-20 17:14:08 +0400

    ++set state replacement

--- lib/utils/common_widgets/setstate_replacement.dart
+++ lib/utils/common_widgets/setstate_replacement.dart
@@ -3,11 +3,26 @@ import 'package:bloc/bloc.dart';
 class ToggleAnything extends Cubit<bool> {
   ToggleAnything() : super(false); // <=== Initial State
   bool anyCondition = false;
+
   void changeState() {
-    if (anyCondition) { // change state according to condition
+    if (anyCondition) {
+      // change state according to condition
       emit(true);
     } else {
       emit(false);
+    }
+  }
+}
+
+class ChangeStringData extends Cubit<String> {
+  ChangeStringData() : super("");
+  bool anyCondition = false;
+
+  void changeString() {
+    if (anyCondition) {
+      emit("Hello");
+    } else {
+      emit("Hai");
     }
   }
 }
